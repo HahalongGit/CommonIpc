5f642fded4aa7460e2538c7c11aa695dd9fa8bcf
liulonglong <729665268@qq.com>
2019-06-16 15:54:14 +0800

    创建test分支 :# Please enter the commit message for your changes. Lines starting

---
+++ app/src/main/java/com/digua/ipctest/text/Test.java
@@ -0,0 +1,21 @@
+package com.digua.ipctest.text;
+
+/**
+ * Created by longlong on 2019/6/16.
+ *
+ * @ClassName: Test
+ * @Description:
+ * @Date 2019/6/16
+ */
+
+public class Test {
+    private String branch2;
+
+    public String getBranch2() {
+        return branch2;
+    }
+
+    public void setBranch2(String branch2) {
+        this.branch2 = branch2;
+    }
+}
