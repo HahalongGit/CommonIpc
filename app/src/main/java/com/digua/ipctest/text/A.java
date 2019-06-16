9a7f0368c063165fbd3564e7e5b0144bd0a3799e
liulonglong <729665268@qq.com>
2019-06-16 15:44:25 +0800

    提交v1.0.0

---
+++ app/src/main/java/com/digua/ipctest/text/A.java
@@ -0,0 +1,21 @@
+package com.digua.ipctest.text;
+
+/**
+ * Created by longlong on 2019/6/16.
+ *
+ * @ClassName: A
+ * @Description:
+ * @Date 2019/6/16
+ */
+
+public class A {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
